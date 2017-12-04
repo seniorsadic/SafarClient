a8ac90e8075a74a1486eec0cbd7878bfdc95b1a7
seniorsadic <33372962+seniorsadic@users.noreply.github.com>
2017-12-04 12:25:55 +0100

    comm

--- src/app/reporting/depot/depot.component.ts
+++ src/app/reporting/depot/depot.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
 
-const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+const URL = '../../../image/';
 
 @Component({
   selector: 'app-depot',
